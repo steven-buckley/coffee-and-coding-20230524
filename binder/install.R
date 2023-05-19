7f2afb133f7f3f8c5c974b1a618029d8c360acd7
Steven Buckley <steven.buckley@nhs.net>
2023-05-19 10:26:26 +0100

    Isolate files for individual coffee and coding session

---
+++ binder/install.R
@@ -0,0 +1,10 @@
+install.packages(
+  c(
+    "dplyr",
+    "lubridate",
+	"devtools"
+  )
+)
+
+# Use devtools to install packages not on CRAN, e.g. from Github
+devtools::install_github("nhsbsa-data-analytics/personMatchR")
